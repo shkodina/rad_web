1d7a9988fc8598989acec17bbb869cf31918af4a
shkodina <blobby@yandex.ru>
2016-03-05 13:07:18 +0300

    для вывода строк сообщений используется директива!

--- public/script.js
+++ public/script.js
@@ -2,65 +2,70 @@
 
  */
 angular.module('exercises', [
-    'ui.router',
-    'ngAnimate',
-    'ngMessages'
-])
-.factory('valueService', function ($rootScope, $location) {
+        'ui.router',
+        'ngAnimate',
+        'ngMessages'
+    ])
+    .factory('valueService', function ($rootScope, $location) {
 
-	var socket = io.connect();
-    
-	var compass = {};
-	$.getJSON("compass.json", function(json) {
-		compass = json;
-    });
+        var socket = io.connect();
 
-    return {
-        b: function () {
-        },
-		getSocket : function(){
-			return socket;
-		},
-		getCompass : function(){
-			return compass;
-		}
-    }
-})
-.controller('MainCtrl', function ($scope, $rootScope, valueService) {
-    this.title = 'RADICO@Web';
+        var compass = {};
+        $.getJSON("compass.json", function (json) {
+            compass = json;
+        });
 
-    this.meas = {};
-    this.cur_mea = {};
-
-    this.setCurrentItem = function(item){
-        this.cur_mea = item;
-    };
-	
-	    valueService.getSocket().on('printMeas', function (mes) {
-        //console.log('printMeas', mes);
-
-        //console.log("compass", compass); // this will show the info it in firebug console
-
-        for(var index in mes) {
-            //console.log("mes index",compass[mes[index].way]);
-            mes[index].compass = valueService.getCompass()[mes[index].way];
-            //console.log("mes index compass",mes[index].compass);
-
-            mes[index].quality_text = (mes[index].quality == "0") ? "Измерение недостоверно" : " ";
-
-            if (mes[index].threshold == "1"){
-                mes[index].warning = true;
-            }
-
-            if (mes[index].threshold == "2"){
-                mes[index].alert = true;
+        return {
+            b: function () {
+            },
+            getSocket: function () {
+                return socket;
+            },
+            getCompass: function () {
+                return compass;
             }
         }
+    })
+    .controller('MainCtrl', function ($scope, $rootScope, valueService) {
+        this.title = 'RADICO@Web';
 
-        $scope.main.meas = mes;
-        $scope.$apply();
-		
-		console.log("printMeas","All Ok");
-    });
-})
+        this.meas = {};
+        this.cur_mea = {};
+
+        this.setCurrentItem = function (item) {
+            this.cur_mea = item;
+        };
+
+        valueService.getSocket().on('printMeas', function (mes) {
+            //console.log('printMeas', mes);
+
+            //console.log("compass", compass); // this will show the info it in firebug console
+
+            for (var index in mes) {
+                //console.log("mes index",compass[mes[index].way]);
+                mes[index].compass = valueService.getCompass()[mes[index].way];
+                //console.log("mes index compass",mes[index].compass);
+
+                mes[index].quality_text = (mes[index].quality == "0") ? "Измерение недостоверно" : " ";
+
+                if (mes[index].threshold == "1") {
+                    mes[index].warning = true;
+                }
+
+                if (mes[index].threshold == "2") {
+                    mes[index].alert = true;
+                }
+            }
+
+            $scope.main.meas = mes;
+            $scope.$apply();
+
+            console.log("printMeas", "All Ok");
+        });
+    })
+    .directive('item', function () {
+        return {
+            templateUrl: 'item.tmpl.html'
+        }
+    })
 ;
